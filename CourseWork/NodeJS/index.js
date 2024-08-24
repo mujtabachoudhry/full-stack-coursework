be77b11fac9a686287e7d53076a54ba0d4173f28
Mujtaba <choudhary.mujtaba@datacubist.com>
2024-08-24 18:51:29 +0300

    nodejs coding along exercise

---
+++ CourseWork/NodeJS/index.js
@@ -0,0 +1,11 @@
+import getPosts, {getPostLength} from './postController.js';
+
+console.log(getPosts());
+
+console.log('Posts length:', getPostLength());
+
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+// console.log('Random number:', generateRandomNumber());
+
+// console.log('Celcius to Fahrenheit:', celciusToFahrenheit(0));
